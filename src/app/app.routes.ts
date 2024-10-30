cac90184eec03b78cbc0e27426ba6382d72d2ca7
Kokou Godwin TCHAKPANA <116899093+itsKokou@users.noreply.github.com>
2024-10-30 09:11:38 +0000

    reprise

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren : ()=>import("./public/public.module").then(mod=>mod.PublicModule)
+    },
+    {
+        path: 'admin',
+        loadChildren : ()=>import("./secure/pages/super-admin/super-admin.module").then(mod=>mod.SuperAdminModule)
+    },
+    {
+        path: 'gestion',
+        loadChildren : ()=>import("./secure/pages/admin-gestion/admin-gestion.module").then(mod=>mod.AdminGestionModule)
+    },
+    {
+        path: 'trader',
+        loadChildren : ()=>import("./secure/pages/trader/trader.module").then(mod=>mod.TraderModule)
+    },
+    {
+        path: 'validateur',
+        loadChildren : ()=>import("./secure/pages/validateur/validateur.module").then(mod=>mod.ValidateurModule)
+    },
+];
--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children:[
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
